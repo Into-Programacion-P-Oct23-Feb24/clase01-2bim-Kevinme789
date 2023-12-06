29b27f62a346a0c7c4e73b219deccb4b1081aabc
kevin <porkevin743@gmail.com>
2023-12-06 16:33:32 -0500

    Actividad en clase

--- ManejoCicloFor/src/paquete03/Ejemplo03.java
+++ ManejoCicloFor/src/paquete03/Ejemplo03.java
@@ -5,16 +5,37 @@
  */
 package paquete03;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Ejemplo03 {
-    
+
     public static void main(String[] args) {
-        
-        for (int i = 0; i < 10; i++) {
-            
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        String cadenaFinal = "";
+        String nombre;
+        String posicion;
+        int edad;
+        double estatura;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Ingrese el nombre del jugador");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese la posicion del jugador");
+            posicion = entrada.nextLine();
+            System.out.println("Ingrese la edad del jugador");
+            edad = entrada.nextInt();
+            System.out.println("Ingrese la estatura del jugador");
+            estatura = entrada.nextDouble();
+            entrada.nextLine();
+            cadenaFinal = String.format("%s%d.%s -%s-, edad %d,estatura %.2f\n",
+                     cadenaFinal,i, nombre, posicion, edad, estatura);
+
         }
+         System.out.printf("%s", cadenaFinal);
     }
 }
